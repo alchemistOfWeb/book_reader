fbc14a5625f74586f96cf91ab9e4d66593be9f69
AlchemistOfWeb <nikitakzncv@gmail.com>
2021-06-23 05:55:29 +0400

    edit seed.sql

--- seed.php
+++
@@ -1,3 +0,0 @@
-<?php
-$sql = 
-    'CREATE TABLE `book_reader`.`books` ( `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT , `title` VARCHAR(255) NULL DEFAULT NULL , `description` TEXT NULL DEFAULT NULL , `path` VARCHAR(1024) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;';
